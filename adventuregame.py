06995b6574de8c62e18d334dcfbb869b55513e00
Ontenous <nathanw@wlhs.wlwv.k12.or.us>
2024-10-30 09:55:12 -0700

    Begun Project

---
+++ adventuregame.py
@@ -0,0 +1,29 @@
+print(f"""
+       __             _,-"~^"-. 
+     _// )      _,-"~`         `. 
+   ." ( /`"-,-"`                 ; 
+  / 6                             ; 
+ /           ,             ,-"     ; 
+(,__.--.      \           /        ; 
+ //'   /`-.\   |          |        `._________ 
+   _.-'_/`  )  )--...,,,___\     \-----------,) 
+ ((("~` _.-'.-'           __`-.   )         // 
+   jgs ((("`             (((---~"`         // 
+                                          ((________________  
+      """)
+
+while True:
+    print(f"Before we begin your adventure, there is a few things you should know."
+          f"There are 5 Adventurers to choose from, each with their own story, "
+          f"class, and more. Kinsuna, Mirajane, Xuri Volk, Pollux, and Erza. You"
+          f"may choose immediately, or you can put 'info' before the character's name"
+          f"to get more information. Thank you for reading.")
+    dnd_class = input("Who would you like to be? ")
+    dnd_class = title(dnd_class)
+    if dnd_class == "Kinsuna":
+        break
+    if dnd_class == "Xuri" or dnd_class == "Xuri Volk":
+        break
+    if dnd_class == "Mirajane"
+        break
+    if dnd_class == "Pollux"
