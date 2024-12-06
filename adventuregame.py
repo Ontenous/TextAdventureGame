e68ddffab6dc165627a478003e5824c1441e9560
Ontenous <nathanw@wlhs.wlwv.k12.or.us>
2024-12-06 09:47:56 -0800

    Kinsuna Dialogue complete and return to deck options

--- adventuregame.py
+++ adventuregame.py
@@ -1,4 +1,4 @@
-
+#Ascii art for title screen
 print(f"""
        __             _,-"~^"-. 
      _// )      _,-"~`         `. 
@@ -11,16 +11,18 @@ print(f"""
  ((("~` _.-'.-'           __`-.   )         // 
    jgs ((("`             (((---~"`         // 
                                           ((________________  
-      """)
-
+""")
+# Pre-Game Choices
 print(f"""
           Before we begin your adventure, there is a few things you should know. 
           There are 5 Adventurers to choose from, each with their own story, 
           class, and more. Kinsuna, Mirajane, Xuri Volk, Pollux, and Erza. You 
           may choose immediately, or you can put 'info' before the character's name 
-          to get more information. Thank you for reading.""")
+          to get more information. Also, for future notes, please try and write your
+          response close to the text, I am kinda bad at programming.
+          Thank you for reading and have fun!""")
 print()
-
+# Character choice, as well as Info
 while True:
     dnd_class = input("Who would you like to be? ")
     dnd_class = dnd_class.title()
@@ -96,7 +98,7 @@ some new friends.
 print()
 print()
 print()
-
+#Dictionary for Opening dialogue
 opening_dialogue = {
     "Kinsuna":"""
 Amidst the large crowds and bellowing laughter, the stench of grog, smoke,
@@ -135,7 +137,7 @@ with the bard currently on stage, tankard's slosh around mead all over the floor
 and the bard’s bright colors and loose fabric grab your attention."""
 
 }
-
+# Checks to see if they match up with a character
 if dnd_class == "Xuri" or dnd_class == "Xuri Volk":
     print(opening_dialogue["Xuri"])
 if dnd_class == "Kinsuna":
@@ -146,22 +148,24 @@ if dnd_class == "Pollux":
     print(opening_dialogue["Pollux"])
 if dnd_class == "Erza":
     print(opening_dialogue["Erza"])
-
+# Variable count before Golden Ram shows up, they go to 3 different places before it does that
 golden_ram = 0
 
+# Naming Scheme: first letter = first letter of character name, second is d for dialogue or loc for location
+# third letter is for the number that the dialogue is, as multiple sections of the game will have dialogue
 
 while True:
     op_loc = input("""
 Upon this ship lies 3 different decks, the Underbelly, The Tween Deck,
 and above deck. Where would you like to go?
-    """)
+    """) # Allows them to decide, though they know where they started
     op_loc = op_loc.title()
     if op_loc == "The Underbelly" or op_loc == "Underbelly":
         print("""
 Scattered barrels, occasionally leaking mead are strewn about,
 as well as various spices, and ship supplies.
         """)
-        golden_ram = golden_ram+1
+        golden_ram = golden_ram+1 # Made choice to go here, count increases
         if dnd_class == "Kinsuna" or dnd_class == "Pollux" or dnd_class == "Mirajane" or dnd_class == "Erza":
             print("""
 A small drow woman sits upon a barrel in the underbelly of the ship. Her once 
@@ -182,17 +186,17 @@ various adventures.
 > Leave                 """)
                     xd1 = input(" ")
                     if xd1 == "What are you doing down here?" or xd1 == "What are you doing down here":
-                        print("Reading. It's pretty loud up there, so uh, could you leave?")
+                        print("Xuri: Reading. It's pretty loud up there, so uh, could you leave?")
                     if xd1 == "What is your name?" or xd1 == "What is your name":
-                        print("Xuri.")
+                        print("Xuri: Xuri.")
                     if xd1 == "Hi" or xd1 == "hi":
-                        print("Hello? ...")
+                        print("Xuri: Hello? ...")
                     if xd1 == "How's that book you're reading?" or xd1 == "Hows that book youre reading":
-                        print("It's alright, a little damp right now.")
+                        print("Xuri: It's alright, a little damp right now.")
                     if xd1 == "Leave" or xd1 == "leave":
                         break
 
-        elif dnd_class == "Xuri" or dnd_class == "Xuri Volk":
+        elif dnd_class == "Xuri" or dnd_class == "Xuri Volk": # They can't talk to themself
             continue
 
     if op_loc == "The Tween Deck" or op_loc == "Tween Deck":
@@ -206,41 +210,66 @@ various adventures.
         He taps his lanterns onto the pile closest to the man, sparkling with bright colors. 
         The man falls out of his chair, as the other bellows a hearty laugh
         """)
-        golden_ram = golden_ram+1
-        if dnd_class == "Kinsuna" or dnd_class == "Erza":
+        golden_ram = golden_ram+1 # ooh it's approaching!
+        if dnd_class == "Kinsuna" or dnd_class == "Erza": # Can't talk to self or each other :/
             continue
         if dnd_class == "Xuri" or dnd_class == "Xuri Volk" or dnd_class == "Pollux" or dnd_class == "Erza":
-            tdd1 = input("Would you like to talk to the Dragonborn or the Bard? (Y/N) ")
-            tdd1 = tdd1.title()
-            kdo1 = 0
-            if tdd1 == "Yes":
-                tdd1 = input("The Dragonborn or the Bard? ")
-                if tdd1 == "The Dragonborn" or tdd1 == "Dragonborn":
-                    print("The angry man begins to storm away, as the gold dragonborn leans back in their chair")
-                    while True:
-                        print(f"""
+            while True:
+                tdd1 = input("Would you like to talk to the Dragonborn or the Bard? (Y/N) ")
+                tdd1 = tdd1.title()
+                kdo1 = 0 # meant to be hidden variable to open up new dialogue interactions
+                if tdd1 == "Yes":
+                    tdd1 = input("The Dragonborn or the Bard? ")
+                    if tdd1 == "The Dragonborn" or tdd1 == "Dragonborn":
+                        print("The angry man begins to storm away, as the gold dragonborn leans back in their chair")
+                        while True:
+                            print(f"""
 {dnd_class}:
 > Hello!
 > What are you doing?
 > What's your name?
 > That guy seems mad
 > Leave                      """)
-                        kd1 = input("")
-                        if kd1 == "Hello!" or kd1 == "Hello":
-                            print("Kinsuna: Hey there")
-                            kdo1 = 1
-                            continue
-                        if kd1 == "What are you doing?" or kd1 == "What are you doing":
-                            print("Kinsuna: Ha, telling fortunes, would you like yours read? (Y/N) ")
-                            kdof = input("")
-                            if kdof ==  "Yes":
-                                print("""
+                            if kdo1 == 1: # check to add to dialogue list, looks cool :D
+                                print("> I'm pretty good, how are you?")
+                            kd1 = input("")
+                            if kd1 == "Hello!" or kd1 == "Hello":
+                                print("Kinsuna: Hey there, how are you?")
+                                kdo1 = 1
+                                continue
+                            if kd1 == "What are you doing?" or kd1 == "What are you doing":
+                                print("Kinsuna: Ha, telling fortunes, would you like yours read? (Y/N) ")
+                                kdof = input("")
+                                if kdof ==  "Yes":
+                                    print("""
 Kinsuna takes out another pile of dust. They get close and let out a small spark of fire.
 The smoke twists and turns into a five pointed star, then dissapates.
 Kinsuna: Looks like we might have a bit of turbulence on this boat... huh.
                                 """)
+                                    continue
+                                if kdof == "No":
+                                    continue
+                            if kd1 == "What's your name?":
+                                print("Kinsuna: My name's Kinsuna, nice to meet you")
                                 continue
+                            if kd1 == "That guy seems mad":
+                                print("""
+Kinsuna:Wasn't a big fan of his fortune, kinda uptight. Was pretty funny 
+        though to seem him fall back and storm off like that.""")
+                                continue
+                            if kd1 == "I'm pretty good, how are you?":
+                                print("""
+Kinsuna: I'm doing alright, least we're close to ending this trip,
+         I have been on here for WAY too long.""")
+                            if kd1 == "Leave":
+                                print("Before you go, would you like to talk to the bard? (Y/N)")
+                                md1s = input("")
+                                break
+# I am so glad that mess of a dialogue is done, they either can leave, or talk to Erza
+
 
                         
-                #if tdd1 == "The Bard" or tdd1 == "Bard":
-                    #pass
+                if tdd1 == "The Bard" or tdd1 == "Bard" or md1s == "Yes":
+                    print("yippee")
+                if tdd1 == "No":
+                    break
